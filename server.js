aeaf0c35f852e884b2ed6e00a08717f8990b2f8e
ayush <ayush@chouksey.com>
2023-06-19 08:45:52 +0530

    users schema changes

--- server.js
+++ server.js
@@ -20,9 +20,10 @@ app.use(logger)
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors(corsOptions))
-// app.use('/',express.static(path.join(__dirname, '/public')))
-app.use(express.static('public'))
+app.use('/',express.static(path.join(__dirname, '/public')))
+// app.use(express.static('public'))
 app.use('/',require('./routes/root'))
+app.use('/users', require('./routes/userRoutes'))
 app.all('*',(req,res) => {
     res.status(404)
     if (req.accepts('html')){
